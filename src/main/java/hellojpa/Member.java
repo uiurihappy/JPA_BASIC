ab7e7a461e8e11faa72fd1558ce4e6f5d16714eb
yunbeom <uiurihappy@sju.ac.kr>
2022-01-02 16:56:53 +0900

    feat: 영속성 컨텍스트

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -10,6 +10,18 @@ public class Member {
     private Long id;
     private String name;
 
+    // JPA에서는 기본적으로 내부적 리플렉션이나 사용되기 때문에 동적으로 객체를 생성해야 하므로
+    // 기본 생성자가 있어야 한다.
+
+    public Member(){
+
+    }
+
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public Long getId() {
         return id;
     }
